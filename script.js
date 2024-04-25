e51b3f38562601f1e11af41ba6b2f575758f220b
verawood langvong <163981873+JeenVerawood@users.noreply.github.com>
2024-04-25 22:53:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showRankingMenu() {
+    container.style.display='block';
+    player_history.style.display='none';
+    rankingmenu.style.backgroundColor='#feb603'; 
+    playinghistory.style.backgroundColor='#ededed';
+}
+
+function showPlayingHistory() {
+    container.style.display='none';
+    player_history.style.display='block';
+    rankingmenu.style.backgroundColor='#ededed'; 
+    playinghistory.style.backgroundColor='#feb603'
+}
